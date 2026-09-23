83c8c913550f0f5cdccedc2803a191b4b9f5f91d
Hiranmai Kuna <kunahiranmai@gmail.com>
2026-09-23 17:50:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function addToCart(product) {
+    alert(product + " added to cart!");
+}
+
+function showProducts() {
+    document.getElementById("products").scrollIntoView({
+        behavior: "smooth"
+    });
+}
